8f837046829a8318fce230f6c65c5d6eda30cc08
Quentin <qdimanche@gmail.com>
2022-05-19 23:01:10 +0200

    [+] footer

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Project = () => {
     return (
         <div>
-            
+            <Navbar/>
+            <Footer/>
         </div>
     );
 };
