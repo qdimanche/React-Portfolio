6542ece9d16c2d3595d99f4ff5d65f9456be0ce5
Quentin <qdimanche@gmail.com>
2022-05-24 21:29:20 +0200

    [+] about content

---
+++ src/components/AboutContent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import screenshotstudlapfigma from "../assets/screenshot-studlap-figma.jpg"
+import screenshotfigmasafecar1 from "../assets/screenshot-figma-safecar-1.png"
+
+const AboutComponent = () => {
+    return (
+        <div className={"w-full my-12 flex items-center justify-center flex-wrap h-full lg:px-0 px-12"}>
+            <div className={"items-center m-auto p-[1rem] max-w-[340px] text-center"}>
+                <h1 className={"font-bold text-2xl"}>Qui je suis ?</h1>
+                <p className={"my-6"}>Je suis un développeur web</p>
+                <Link to={"/contact"}>
+                    <button className={"uppercase bg-orange-400 hover:bg-[#e28336] py-2 px-10 text-black text-lg border border-white"}>Contact</button>
+                </Link>
+            </div>
+
+            <div className={"max-w-[700px] lg:mt-8 mt-8"}>
+                <div className={"grid grid-col-custom-12 relative items-center text-center"}>
+                    <div className={"col-[1_/_span_8] grid-row-custom-1 pt-1/5 z-[6]"}>
+                        <img src={screenshotstudlapfigma} alt="" className={"max-w-[90%] border border-[#333]"}/>
+                    </div>
+                    <div className={"col-[4_/_-1] grid-row-custom-1 z-[5]"}>
+                        <img src={screenshotfigmasafecar1} alt="" className={"max-w-[90%] border border-[#333]"}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AboutComponent;
--- src/components/Work.js
+++ src/components/Work.js
@@ -7,7 +7,7 @@ import WorkCardData from "./WorkCardData";
 const Work = () => {
     return (
         <div className={"lg:py-16 py-12"}>
-            <h1 className={"text-center pt-4 pb-12 text-xl font-bold    "}>Projets</h1>
+            <h1 className={"text-center pt-4 pb-12 text-2xl font-bold    "}>Projets</h1>
             <div className={"container lg:mx-auto m-auto max-w-[90%] grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-[40px]"}>
                 {WorkCardData.map((val, ind)=>{
                     return(
